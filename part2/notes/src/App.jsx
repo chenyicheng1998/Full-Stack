d7a317b78582d020b899ea17593608a6edb101f9
Yicheng CHEN <yicheng4499@gmail.com>
2025-06-09 00:32:25 +0300

    Changing the Importance of Notes

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -32,6 +32,16 @@ const App = () => {
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id === id ? response.data : note))
+    })
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -42,7 +52,7 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
         ))}
       </ul>
       <form onSubmit={addNote}>
