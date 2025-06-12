93ed2e48b1fa5156ef60ee86507393261e74f605
Yicheng CHEN <yicheng4499@gmail.com>
2025-06-12 15:50:23 +0300

    finish exercise 3.2

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,32 +1,44 @@
 const express = require('express')
 const app = express()
 
-let notes = [
+let persons = [
     {
-        id: '1',
-        content: 'HTML is easy',
-        important: true,
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
     {
-        id: '2',
-        content: 'Browser can execute only JavaScript',
-        important: false,
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
     {
-        id: '3',
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        important: true,
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
 ]
 
 app.use(express.json())
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
 })
 
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
+app.get('/info', (request, response) => {
+    const personCount = persons.length
+    const currentTime = new Date()
+
+    response.send(`
+        <p>Phonebook has info for ${personCount} people</p>
+        <p>${currentTime}</p>
+    `)
 })
 
 app.get('/api/notes/:id', (request, response) => {
