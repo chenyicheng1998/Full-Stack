c88d75e7c89f9b1fbf55cbad90254ff7b9ee1e8a
Yicheng CHEN <yicheng4499@gmail.com>
2025-06-12 21:27:47 +0300

    I think I finish exercise 3.11, try to push and test on Render

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -33,6 +33,7 @@ morgan.token('body', (req) => {
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('dist'))
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
