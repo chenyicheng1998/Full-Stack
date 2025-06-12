d95d127ff1447093641541f1e596f0331542c99a
Yicheng CHEN <yicheng4499@gmail.com>
2025-06-12 16:03:01 +0300

    finish exercise 3.6

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -52,38 +52,49 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
-const generateId = () => {
-    const maxId =
-        notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
-    return String(maxId + 1)
-}
-
-app.post('/api/notes', (request, response) => {
-    const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing',
-        })
-    }
-
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId(),
-    }
-
-    notes = notes.concat(note)
-
-    response.json(note)
-})
-
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    notes = notes.filter((note) => note.id !== id)
+    persons = persons.filter((person) => person.id !== id)
 
     response.status(204).end()
 })
+
+const generateId = () => {
+    const maxId =
+        persons.length > 0 ? Math.max(...persons.map((n) => Number(n.id))) : 0
+    return String(maxId + 1)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing',
+        })
+    }
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing',
+        })
+    }
+    const nameExists = persons.some(
+        (person) => person.name === body.name
+    )
+    if (nameExists) {
+        return response.status(400).json({
+            error: 'name must be unique',
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+    persons = persons.concat(person)
+    response.json(person)
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {
