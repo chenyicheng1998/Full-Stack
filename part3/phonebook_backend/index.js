8cfba45fcf3c0739ea076f8849f6be578b6b5472
Yicheng CHEN <yicheng4499@gmail.com>
2025-06-16 22:54:21 +0300

    nothing

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -32,7 +32,7 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use(cors())
-// app.use(express.static('dist'))
+app.use(express.static('dist'))
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
