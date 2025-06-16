efac6a9c24dd4bb63818581bf7e1091d07b28c52
Yicheng CHEN <yicheng4499@gmail.com>
2025-06-16 22:42:38 +0300

    nothing

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-// const cors = require('cors')
+const cors = require('cors')
 
 let persons = [
     {
@@ -31,7 +31,7 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-// app.use(cors())
+app.use(cors())
 // app.use(express.static('dist'))
 
 app.get('/api/persons', (request, response) => {
