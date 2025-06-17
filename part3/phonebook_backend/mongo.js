323fc6d0fb927f384403fc0810f54739097863af
Yicheng CHEN <yicheng4499@gmail.com>
2025-06-17 03:00:34 +0300

    finish exercise 3.22

--- part3/phonebook_backend/mongo.js
+++ part3/phonebook_backend/mongo.js
@@ -9,37 +9,37 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    // Only password given
-    Person.find({}).then(result => {
-        console.log('phonebook:')
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
+  // Only password given
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+  })
 } else if (process.argv.length === 5) {
-    // Password, name, and number given
-    const name = process.argv[3]
-    const number = process.argv[4]
+  // Password, name, and number given
+  const name = process.argv[3]
+  const number = process.argv[4]
 
-    const person = new Person({
-        name,
-        number,
-    })
+  const person = new Person({
+    name,
+    number,
+  })
 
-    person.save().then(() => {
-        console.log(`added ${name} number ${number} to phonebook`)
-        mongoose.connection.close()
-    })
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 
 } else {
-    console.log('Usage:\n  node mongo.js <password>\n  node mongo.js <password> <name> <number>')
-    mongoose.connection.close()
+  console.log('Usage:\n  node mongo.js <password>\n  node mongo.js <password> <name> <number>')
+  mongoose.connection.close()
 }
