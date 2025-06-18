e2998c8d8bdbf744bf1f4f530764be17da7b8ab6
Yicheng CHEN <yicheng4499@gmail.com>
2025-06-18 19:38:36 +0300

    finish exercise 4.12 and Refactoring tests

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -1,15 +1,136 @@
-const { test, after } = require('node:test')
+const { test, beforeEach, after } = require('node:test')
+const assert = require('assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/')
+const initialBlogs = [
+  {
+    title: 'First blog',
+    author: 'Author One',
+    url: 'http://example.com/1',
+    likes: 5
+  },
+  {
+    title: 'Second blog',
+    author: 'Author Two',
+    url: 'http://example.com/2',
+    likes: 10
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+
+test('blogs are returned as json and correct amount', async () => {
+  const response = await api
+    .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+
+  const blogs = response.body
+  console.log('Blogs:', blogs)
+  if (blogs.length < 1) {
+    throw new Error('Expected at least one blog')
+  }
+})
+
+test('blog posts have a unique identifier property named id', async () => {
+  const response = await api.get('/api/blogs')
+
+  response.body.forEach(blog => {
+    assert.ok(blog.id, 'id field is missing')
+    assert.strictEqual(typeof blog.id, 'string')
+  })
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Newly added blog',
+    author: 'Author Three',
+    url: 'http://example.com/3',
+    likes: 15
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)  // Created
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+
+  // Check that total number increased by 1
+  if (response.body.length !== initialBlogs.length + 1) {
+    throw new Error(`Expected ${initialBlogs.length + 1} blogs, got ${response.body.length}`)
+  }
+
+  // Check that the new blog's title is in the list
+  if (!titles.includes('Newly added blog')) {
+    throw new Error('New blog title not found')
+  }
+})
+
+test('if likes property is missing, it will default to 0', async () => {
+  const newBlog = {
+    title: 'Blog without likes',
+    author: 'Author Four',
+    url: 'http://example.com/4'
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  if (response.body.likes !== 0) {
+    throw new Error(`Expected likes to be 0, got ${response.body.likes}`)
+  }
+})
+
+test('blog without title is not added and returns 400', async () => {
+  const newBlog = {
+    author: 'No Title Author',
+    url: 'http://example.com/no-title',
+    likes: 1
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await api.get('/api/blogs')
+  if (blogsAtEnd.body.length !== initialBlogs.length) {
+    throw new Error('Blog without title should not be added')
+  }
+})
+
+test('blog without url is not added and returns 400', async () => {
+  const newBlog = {
+    title: 'No URL Blog',
+    author: 'No URL Author',
+    likes: 1
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await api.get('/api/blogs')
+  if (blogsAtEnd.body.length !== initialBlogs.length) {
+    throw new Error('Blog without url should not be added')
+  }
 })
 
 after(async () => {
