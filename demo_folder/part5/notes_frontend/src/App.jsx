e20730d78643ac187b797bcd020f9f27bc8217ed
Yicheng CHEN <yicheng4499@gmail.com>
2025-06-21 05:54:35 +0300

    Creating new notes

--- demo_folder/part5/notes_frontend/src/App.jsx
+++ demo_folder/part5/notes_frontend/src/App.jsx
@@ -67,6 +67,8 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      noteService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
