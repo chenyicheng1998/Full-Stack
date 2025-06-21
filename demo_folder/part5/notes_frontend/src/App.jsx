b294a2be3757e7b6313c775100ebbc97bac5267b
Yicheng CHEN <yicheng4499@gmail.com>
2025-06-21 05:57:35 +0300

    Saving the token to the browser's local storage

--- demo_folder/part5/notes_frontend/src/App.jsx
+++ demo_folder/part5/notes_frontend/src/App.jsx
@@ -15,6 +15,15 @@ const App = () => {
   const [user, setUser] = useState(null)
 
   useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
+  useEffect(() => {
     noteService
       .getAll()
       .then(initialNotes => {
@@ -67,7 +76,9 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
-
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
       noteService.setToken(user.token)
       setUser(user)
       setUsername('')
