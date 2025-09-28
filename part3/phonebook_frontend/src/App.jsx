02d9b6161ecf2d3a1795202c96e57bce313e93b5
Yicheng CHEN <yicheng4499@gmail.com>
2025-09-28 17:13:03 +0300

    try to deploy phonebook backend, exercise 3.10

---
+++ part3/phonebook_frontend/src/App.jsx
@@ -0,0 +1,143 @@
+import { useState, useEffect } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+import Notification from './components/Notification'
+import personService from './services/persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState(null)
+  const [notificationType, setNotificationType] = useState('success')
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
+
+  const showNotification = (message, type = 'success') => {
+    setNotificationMessage(message)
+    setNotificationType(type)
+    setTimeout(() => {
+      setNotificationMessage(null)
+    }, 5000)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const existingPerson = persons.find(person => person.name === newName)
+    
+    if (existingPerson) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        const changedPerson = { ...existingPerson, number: newNumber }
+        
+        personService
+          .update(existingPerson.id, changedPerson)
+          .then(returnedPerson => {
+            setPersons(persons.map(person => person.id !== existingPerson.id ? person : returnedPerson))
+            setNewName('')
+            setNewNumber('')
+            showNotification(`Updated ${returnedPerson.name}`)
+          })
+          .catch(error => {
+            showNotification(
+              `Information of ${existingPerson.name} has already been removed from server`,
+              'error'
+            )
+            setPersons(persons.filter(p => p.id !== existingPerson.id))
+            setNewName('')
+            setNewNumber('')
+          })
+      }
+      return
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+        showNotification(`Added ${returnedPerson.name}`)
+      })
+      .catch(error => {
+        showNotification(`Failed to add ${newName}`, 'error')
+      })
+  }
+
+  const deletePerson = (id) => {
+    const person = persons.find(p => p.id === id)
+    
+    if (window.confirm(`Delete ${person.name}?`)) {
+      personService
+        .remove(id)
+        .then(() => {
+          setPersons(persons.filter(p => p.id !== id))
+          showNotification(`Deleted ${person.name}`)
+        })
+        .catch(error => {
+          showNotification(
+            `Information of ${person.name} has already been removed from server`,
+            'error'
+          )
+          setPersons(persons.filter(p => p.id !== id))
+        })
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const personsToShow = searchTerm === ''
+    ? persons
+    : persons.filter(person =>
+      person.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Notification message={notificationMessage} type={notificationType} />
+
+      <Filter searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
+
+      <h3>Add a new</h3>
+
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons personsToShow={personsToShow} deletePerson={deletePerson} />
+    </div>
+  )
+}
+
+export default App
---
+++ part3/phonebook_frontend/src/components/Persons.jsx
@@ -0,0 +1,17 @@
+import Person from './Person'
+
+const Persons = ({ personsToShow, deletePerson }) => {
+  return (
+    <ul>
+      {personsToShow.map(person =>
+        <Person 
+          key={person.id} 
+          person={person} 
+          deletePerson={() => deletePerson(person.id)}
+        />
+      )}
+    </ul>
+  )
+}
+
+export default Persons
