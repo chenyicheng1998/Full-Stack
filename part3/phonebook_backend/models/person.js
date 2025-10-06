b42aed75dcb11bec07da21207dbd252004bf9dc6
Yicheng CHEN <yicheng4499@gmail.com>
2025-10-06 05:51:41 +0300

    finish exercise 3.21

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -23,6 +23,15 @@ const personSchema = new mongoose.Schema({
   number: {
     type: String,
     required: true,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        // 格式：2-3位数字 + "-" + 数字
+        // 例如：09-1234556 或 040-22334455
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number! Phone number must be in format: 09-1234556 or 040-22334455`
+    }
   }
 })
 
