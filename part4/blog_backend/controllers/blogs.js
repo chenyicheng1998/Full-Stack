652e285d1ce944b6af463af7ac9a063deecbf270
Yicheng CHEN <yicheng4499@gmail.com>
2025-10-09 10:22:28 +0300

    finish exercise 4.22

--- part4/blog_backend/controllers/blogs.js
+++ part4/blog_backend/controllers/blogs.js
@@ -2,23 +2,21 @@ const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   const body = request.body
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
+  // get user from request object
+  const user = request.user
 
   if (!user) {
-    return response.status(400).json({ error: 'UserId missing or not valid' })
+    return response.status(401).json({ error: 'token invalid' })
   }
 
   const blog = new Blog({
@@ -36,15 +34,12 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
   try {
-    // Check if token is provided
-    if (!request.token) {
-      return response.status(401).json({ error: 'token missing' })
-    }
+    // get user from request object
+    const user = request.user
 
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!decodedToken.id) {
+    if (!user) {
       return response.status(401).json({ error: 'token invalid' })
     }
 
@@ -54,7 +49,7 @@ blogsRouter.delete('/:id', async (request, response) => {
     }
 
     // Check if the user is the creator of the blog
-    if (blog.user.toString() !== decodedToken.id.toString()) {
+    if (blog.user.toString() !== user._id.toString()) {
       return response.status(403).json({ error: 'permission denied' })
     }
 
