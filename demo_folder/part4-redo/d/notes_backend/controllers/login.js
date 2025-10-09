790a7a496c2749bbe18f87ce0d1ec974d4dc3439
Yicheng CHEN <yicheng4499@gmail.com>
2025-10-09 09:01:38 +0300

    finish part 4d

--- demo_folder/part4-redo/d/notes_backend/controllers/login.js
+++ demo_folder/part4-redo/d/notes_backend/controllers/login.js
@@ -7,8 +7,9 @@ loginRouter.post('/', async (request, response) => {
   const { username, password } = request.body
 
   const user = await User.findOne({ username })
-  const passwordCorrect =
-    user === null ? false : await bcrypt.compare(password, user.passwordHash)
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash)
 
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
@@ -18,12 +19,19 @@ loginRouter.post('/', async (request, response) => {
 
   const userForToken = {
     username: user.username,
-    id: user._id
+    id: user._id,
   }
 
-  const token = jwt.sign(userForToken, process.env.SECRET)
+  // token expires in 60*60 seconds, that is, in one hour
+  const token = jwt.sign(
+    userForToken,
+    process.env.SECRET,
+    { expiresIn: 60 * 60 }
+  )
 
-  response.status(200).send({ token, username: user.username, name: user.name })
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
 })
 
 module.exports = loginRouter
