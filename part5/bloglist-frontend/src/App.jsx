bb5059e9a89e8ead0a4458affdd65a7aa308f9b0
Yicheng CHEN <yicheng4499@gmail.com>
2025-10-11 11:36:54 +0300

    finish exercise 5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -131,9 +131,11 @@ const App = () => {
       <Notification message={notification} type={notificationType} />
       <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
       {blogForm()}
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
-      )}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
+        )}
     </div>
   )
 }
